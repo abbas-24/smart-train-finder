c8a3ecfbc0c5f16e3d9370a5719ecf3de1abc0a2
abbas-24 <96183692+abbas-24@users.noreply.github.com>
2025-06-02 21:14:04 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,8 @@ export default function App() {
 
   // triggered when form is submitted
   const handleSearch = async ({ tripType, departureDate, returnDate, overnightStays, filter, direction }) => {
-    const response = await fetch('http://localhost:4000/api/search', {
+    const response = await fetch('https://smart-train-finder.onrender.com/api/search', {
+    //const response = await fetch('http://localhost:4000/api/search', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
